7ff7bd6232ddb15029f3d7b542c82235cd21959a
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-06-13 13:00:43 -0700

    initalize

---
+++ assets/js/zodiac.js
@@ -0,0 +1,14 @@
+let zodiacSign = [
+    "Aquarius",
+    "Pisces",
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+]
